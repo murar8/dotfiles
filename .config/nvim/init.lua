2652cad7d15cfe281799927791d65399c7da2c97
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-02-06 20:33:56 +0000

    set up telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,6 +128,27 @@ require("lazy").setup({
         },
     },
     {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#mapping-esc-to-quit-in-insert-mode
+                        i = { ["<esc>"] = actions.close },
+                    },
+                },
+            })
+
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        end,
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
