35abaf7a60a5b481b832477fc0fd0704a6d737ae
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-07-21 23:03:30 +0200

    some setup for neovim

---
+++ .config/nvim/lua/user/ui/ui.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup()
+            vim.cmd.colorscheme("gruvbox")
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = { theme = "gruvbox" },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            ---@diagnostic disable-next-line: missing-fields
+            configs.setup({ auto_install = true, highlight = { enable = true }, indent = { enable = true } })
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        end,
+    },
+}
