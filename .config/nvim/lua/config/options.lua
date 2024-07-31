67f75046161e1659f78af35cac334f261fea604a
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-07-31 22:25:22 +0200

    nvim improvements

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,3 @@ vim.opt.gdefault = true
 
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
-
-vim.opt.guifont = "Fira Code:h12"
-vim.g.neovide_cursor_vfx_mode = "ripple"
-vim.g.neovide_cursor_animation_length = 0.01
-vim.g.neovide_cursor_smooth_blink = true
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,8 +1,16 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	opts = {
+		close_if_last_window = true,
 		filesystem = {
 			bind_to_cwd = true,
+			follow_current_file = { enabled = true },
+			filtered_items = {
+				visible = false,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_hidden = false,
+			},
 		},
 	},
 }
