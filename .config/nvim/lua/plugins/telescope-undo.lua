5dd0745eaeb26901cb915d5c6ae25408b98e403f
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-08 08:53:43 +0200

    switch to lazy extras

--- .config/nvim/lua/plugins/telescope-undo.lua
+++ .config/nvim/lua/plugins/telescope-undo.lua
@@ -1,14 +1,15 @@
 return {
-	"debugloop/telescope-undo.nvim",
-	opts = {},
-	dependencies = {
-		{ "nvim-telescope/telescope.nvim" },
+	"nvim-telescope/telescope.nvim",
+	optional = true,
+	specs = {
+		"debugloop/telescope-undo.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>h", "<cmd>Telescope undo<cr>", desc = " Undo History" },
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
+		end,
 	},
-	keys = {
-		{ "<leader>hu", "<cmd>Telescope undo<cr>", desc = " Undo History" },
-	},
-	config = function(_, opts)
-		require("telescope").setup(opts)
-		require("telescope").load_extension("undo")
-	end,
 }
