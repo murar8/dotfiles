4c4b861af9120fee4c6764a91e2408d88e1f9aea
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-09 11:47:33 +0200

    set up more plugins

---
+++ .config/nvim/lua/plugins/lspkind.lua
@@ -0,0 +1,13 @@
+return {
+	"onsails/lspkind.nvim",
+	config = function()
+		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		cmp.setup({
+			---@diagnostic disable-next-line: missing-fields
+			formatting = {
+				format = lspkind.cmp_format({ mode = "symbol" }),
+			},
+		})
+	end,
+}
