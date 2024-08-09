0a3a53b889db0c645ba1038cf83807309ed73f18
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-09 11:47:33 +0200

    set up more plugins

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {},
+	lazy = false,
+	keys = {
+		{ "<leader>He", "<cmd>Hardtime enable<cr>", desc = "Enable Hardtime" },
+		{ "<leader>Hd", "<cmd>Hardtime disable<cr>", desc = "Disable Hardtime" },
+		{ "<leader>Ht", "<cmd>Hardtime toggle<cr>", desc = "Toggle Hardtime" },
+	},
+}
