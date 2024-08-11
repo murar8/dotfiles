a58f3f9719d3974fcf7fd72c6ffb60737176f9d4
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-11 16:51:33 +0200

    some optimization

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -10,5 +10,14 @@ return {
 				hide_hidden = false,
 			},
 		},
+		event_handlers = {
+			-- https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/Recipes#auto-close-on-open-file
+			{
+				event = "file_open_requested",
+				handler = function()
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+		},
 	},
 }
