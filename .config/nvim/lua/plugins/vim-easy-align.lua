6fee580ccf8cfb6e83efe6928d6d2f701546ee5b
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-13 14:35:30 +0200

    add vim-easy-align

---
+++ .config/nvim/lua/plugins/vim-easy-align.lua
@@ -0,0 +1,7 @@
+return {
+	"junegunn/vim-easy-align",
+	keys = {
+		{ "ga", "<Plug>(EasyAlign)", mode = "x" }, -- Start interactive EasyAlign in visual mode (e.g. vipga)
+		{ "ga", "<Plug>(EasyAlign)", mode = "n" }, -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
+	},
+}
