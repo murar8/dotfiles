a362c97e44814f750feb18d4046dbf15e686ca89
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-15 01:05:45 +0200

    set up tmux

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		if vim.opt.buftype:get() == "terminal" then
+			vim.cmd(":startinsert")
+		end
+	end,
+})
