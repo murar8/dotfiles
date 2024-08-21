b2f2e608edf7196a89e17d2366101d1f02e1e989
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-08-21 09:01:44 +0200

    some cleanup

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,3 @@ vim.opt.gdefault = true
 
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
-
-vim.g.root_spec = {
-	{ ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "lazyvim.json" },
-	"lsp",
-	"cwd",
-}
