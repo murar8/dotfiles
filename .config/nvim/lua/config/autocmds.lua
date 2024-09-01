d1010ce4192e989124eae2d02d18abb6652e3fc2
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-09-01 21:13:08 +0200

    remove dotfile git helper

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -14,23 +14,3 @@ vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
 		end
 	end,
 })
-
-vim.api.nvim_create_autocmd({ "DirChanged" }, {
-	pattern = { "*" },
-	callback = function()
-		local root = LazyVim.root()
-		local work_tree = vim.fn.expand("~")
-		local git_dir = work_tree .. "/.dotfiles"
-		local command =
-			{ "git", "--work-tree=" .. work_tree, "--git-dir=" .. git_dir, "ls-files", "--error-unmatch", root }
-		vim.fn.system(command)
-		if vim.v.shell_error == 0 and not vim.uv.fs_stat(root .. "/.git") then
-			vim.api.nvim_echo({ { "Setting git worktree to track bare dotfiles repo." } }, true, {})
-			vim.env.GIT_WORK_TREE = work_tree
-			vim.env.GIT_DIR = git_dir
-		else
-			vim.env.GIT_WORK_TREE = vim.v.null
-			vim.env.GIT_DIR = vim.v.null
-		end
-	end,
-})
