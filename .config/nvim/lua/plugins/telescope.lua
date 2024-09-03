815428ca1d4c823a247d0210a19f00ba5e446f35
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-09-03 08:18:50 +0200

    cleanup crud

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,6 @@ return {
 	"nvim-telescope/telescope.nvim",
 	optional = true,
 	keys = {
-		{ "<leader><space>", "<cmd>Telescope resume<cr>", desc = "Resume" },
+		{ "<leader>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
 	},
 }
