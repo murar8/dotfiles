97c47c6e54c2eb20c5173822226956fd68d69c50
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-09-03 20:32:02 +0200

    swap telescope with fzf

--- .config/nvim/lua/plugins/hbac.lua
+++ .config/nvim/lua/plugins/hbac.lua
@@ -6,18 +6,5 @@ return {
 			close_buffers_with_windows = true,
 			close_command = LazyVim.ui.bufremove,
 		},
-		specs = {
-			{
-				"nvim-telescope/telescope.nvim",
-				optional = true,
-				keys = {
-					{ "<leader>fh", "<cmd>Telescope hbac buffers<CR>", desc = "HBAC buffers" },
-				},
-				config = function(_, opts)
-					require("telescope").setup(opts)
-					require("telescope").load_extension("hbac")
-				end,
-			},
-		},
 	},
 }
