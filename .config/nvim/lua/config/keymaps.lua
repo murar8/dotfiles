54335ce34b1b7ee24d72825db46577e4a026f3cb
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-09-26 09:37:06 +0200

    some cleanup

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,3 @@
 LazyVim.safe_keymap_set("n", "<leader><BS>", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+LazyVim.safe_keymap_set("t", "<C-\\>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
 vim.keymap.del("t", "<esc><esc>")
