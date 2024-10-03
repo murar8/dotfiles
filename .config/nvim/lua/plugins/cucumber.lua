69a779750379ee8da7a5e077c004c1f2906d088b
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-10-03 08:26:19 +0200

    sync

--- .config/nvim/lua/plugins/cucumber.lua
+++ .config/nvim/lua/plugins/cucumber.lua
@@ -1,25 +1,7 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		optional = true,
-		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "cucumber-language-server")
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		optional = true,
-		opts = {
-			servers = {
-				cucumber_language_server = {
-					settings = {
-						cucumber = {
-							features = { "test/features/**/*.feature" },
-							glue = { "test/steps/**/*.ts" },
-						},
-					},
-				},
-			},
-		},
-	},
+	"williamboman/mason.nvim",
+	optional = true,
+	opts = function(_, opts)
+		table.insert(opts.ensure_installed, "cucumber-language-server")
+	end,
 }
