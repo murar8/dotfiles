c847ff064fddcc7cdd2d52ae2117841edb290384
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-10-10 14:52:36 +0200

    set up vscode

---
+++ .config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,9 @@
+return {
+	"jay-babu/mason-nvim-dap.nvim",
+	optional = true,
+	opts = {
+		ensure_installed = {
+			"codelldb",
+		},
+	},
+}
---
+++ .config/nvim/lua/plugins/vscode-multi-cursor.lua
@@ -0,0 +1,7 @@
+
+return {
+  'vscode-neovim/vscode-multi-cursor.nvim',
+  event = 'VeryLazy',
+  cond = not not vim.g.vscode,
+  opts = {},
+}
