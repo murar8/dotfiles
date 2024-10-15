667a433d58ed44ec9a05945c73ca861fced6bd4d
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-10-15 20:35:48 +0200

    config cleanup

--- .config/nvim/lua/plugins/protobuf.lua
+++ .config/nvim/lua/plugins/protobuf.lua
@@ -9,16 +9,6 @@ return {
 		end,
 	},
 	{
-		"nvimtools/none-ls.nvim",
-		optional = true,
-		opts = function(_, opts)
-			local nls = require("null-ls")
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, nls.builtins.formatting.clang_format)
-			table.insert(opts.sources, nls.builtins.diagnostics.buf)
-		end,
-	},
-	{
 		"stevearc/conform.nvim",
 		optional = true,
 		opts = {
