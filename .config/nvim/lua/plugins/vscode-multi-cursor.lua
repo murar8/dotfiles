f062c36702fe14c210c7ba333d60771bb4ca028c
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-10-21 15:07:55 +0200

    set up vscode

---
+++ .config/nvim/lua/plugins/vscode-multi-cursor.lua
@@ -0,0 +1,39 @@
+return {
+	"vscode-neovim/vscode-multi-cursor.nvim",
+	event = "VeryLazy",
+	cond = not not vim.g.vscode,
+	opts = {},
+	keys = {
+		{
+			"<c-n>",
+			mode = { "n", "x", "i" },
+			function()
+				require("vscode-multi-cursor").addSelectionToNextFindMatch()
+			end,
+		},
+		{
+			"<c-s-l>",
+			mode = { "n", "x", "i" },
+			function()
+				require("vscode-multi-cursor").selectHighlights()
+			end,
+		},
+	},
+}
+
+--[[ keybindings.json (VSCode)
+[
+  {
+    "args": "<c-n>",
+    "command": "vscode-neovim.send",
+    "key": "ctrl+n",
+    "when": "editorFocus && neovim.init"
+  },
+  {
+    "args": "<c-s-l>",
+    "command": "vscode-neovim.send",
+    "key": "ctrl+shift+l",
+    "when": "editorFocus && neovim.init"
+  }
+]
+--]]
