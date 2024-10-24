d6b6cb325a7f3832ab00b1dcabead298a758a63a
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-10-24 20:12:57 +0200

    fix dot autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -19,11 +19,17 @@ vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
 	pattern = { "*" },
 	callback = function()
 		local blacklist = { ".dotfiles", ".cache", ".local" }
-		local root_dir = LazyVim.root()
+		local root_dir = vim.fn.fnamemodify(vim.fn.argv()[1] or vim.fn.getcwd(), ":p")
 		local home_dir = vim.fn.expand("~")
 		local is_blacklisted = vim.tbl_contains(blacklist, root_dir:match("^" .. home_dir .. "/([^/]+)"))
 		local is_home_hidden_folder = root_dir:match("^" .. home_dir .. "/%.") ~= nil
-		if is_home_hidden_folder and not is_blacklisted then
+		local is_git_repo = vim.fn.isdirectory(root_dir .. "/.git") == 1
+		print("Root directory:", root_dir)
+		print("Home directory:", home_dir)
+		print("Is blacklisted:", is_blacklisted)
+		print("Is home hidden folder:", is_home_hidden_folder)
+		print("Is git repo:", is_git_repo)
+		if is_home_hidden_folder and not is_blacklisted and not is_git_repo then
 			if vim.env.GIT_WORK_TREE ~= home_dir then
 				vim.api.nvim_echo({ { "Setting git worktree to track bare dotfiles repo." } }, true, {})
 				vim.env.GIT_WORK_TREE = home_dir
