46e9eba7cbb23fcbad8c59b909bdd92e198cd2f6
Lorenzo Murarotto <murar8@proton.me>
2025-06-16 17:07:13 +0200

    add cursor config

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,24 @@
 return {
 	"neovim/nvim-lspconfig",
 	optional = true,
-	opts = {
-		inlay_hints = {
-			enabled = false,
-		},
-		servers = {
-			denols = {},
-		},
-	},
+	opts = function(_, opts)
+		opts.inlay_hints = { enabled = false }
+
+		opts.servers.denols = {}
+
+		-- Check if the ESP-IDF environment variable is set
+		local esp_idf_path = os.getenv("IDF_PATH")
+		if esp_idf_path then
+			-- for esp-idf
+			opts.servers.clangd = {
+				cmd = {
+					"/home/lmurarotto/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin/clangd",
+					"--background-index",
+					"--query-driver=**",
+				},
+			}
+		end
+
+		return opts
+	end,
 }
