df20cb75bc06877928764f143deaaa76d1e8d844
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-07-15 16:47:17 +0200

    remove double esc

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/snacks.nvim",
+	---@param opts snacks.Config
+	opts = function(_, opts)
+		local _ = Snacks
+		opts.terminal.win.keys.term_normal = false -- disable entering terminal mode on <esc><esc>
+		return opts
+	end,
+}
