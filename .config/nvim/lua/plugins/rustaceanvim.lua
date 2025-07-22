c7658683e9a3a60b9bbd6b52fe9a96f21ecef74c
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-07-22 22:52:42 +0200

    sync

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,22 +1,19 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    opts = {
-      server = {
-        settings = {
-          ["rust-analyzer"] = {
-            procMacro = {
-              enable = true,
-              ignored = {
-                -- Remove "async-trait" from the default ignored list
-                -- Keep other commonly problematic macros
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+	{
+		"mrcjkb/rustaceanvim",
+		opts = {
+			server = {
+				settings = {
+					["rust-analyzer"] = {
+						procMacro = {
+							enable = true,
+							-- https://github.com/LazyVim/LazyVim/issues/6111
+							ignored = {},
+						},
+					},
+				},
+			},
+		},
+	},
 }
+
