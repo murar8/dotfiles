179c513a4401f857bb0dd6da1d0be7226a842405
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-07-30 10:50:33 +0200

    set up multi cursor

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,6 @@
 vim.g.ai_cmp = false
 vim.g.snacks_animate = false
+vim.g.root_spec = { { ".git" }, "cwd" }
 vim.opt.gdefault = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 vim.opt.undofile = true
