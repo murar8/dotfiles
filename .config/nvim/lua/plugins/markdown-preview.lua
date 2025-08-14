47c2427965f931bf024d435ee6e2fb963d92f001
Lorenzo Murarotto <murar8@proton.me>
2025-08-14 13:45:06 +0200

    sync

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,20 +1,8 @@
--- See https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2254280534
+-- https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2510492642
 return {
 	"iamcco/markdown-preview.nvim",
 	optional = true,
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
-	build = function(plugin)
-		if vim.fn.executable("npx") then
-			vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-		else
-			vim.cmd([[Lazy load markdown-preview.nvim]])
-			vim.fn["mkdp#util#install"]()
-		end
-	end,
-	init = function()
-		if vim.fn.executable("npx") then
-			vim.g.mkdp_filetypes = { "markdown" }
-		end
-	end,
+	build = ":call mkdp#util#install()",
 }
