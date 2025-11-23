48f5d0149485e3071303e8aac5e9fcd0282227d7
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-11-23 15:46:50 +0100

    update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "catppuccin/nvim", enabled = false },
-	{ "ellisonleao/gruvbox.nvim" },
+	{
+
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+	},
 	{
 		"LazyVim/LazyVim",
 		optional = true,
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
