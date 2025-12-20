71147fbea6c66c63b24525402e1460f76921eb30
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-12-20 20:55:12 +0100

    set up baredot

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -15,25 +15,3 @@ vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufEnter" }, {
-	pattern = { "*" },
-	callback = function()
-		local blacklist = { ".dotfiles", ".cache", ".local" }
-		local root_dir = vim.fn.fnamemodify(vim.fn.argv()[1] or vim.fn.getcwd(), ":p")
-		local home_dir = vim.fn.expand("~")
-		local is_blacklisted = vim.tbl_contains(blacklist, root_dir:match("^" .. home_dir .. "/([^/]+)"))
-		local is_home_hidden_folder = root_dir:match("^" .. home_dir .. "/%.") ~= nil
-		local is_git_repo = vim.fn.isdirectory(root_dir .. "/.git") == 1
-		if is_home_hidden_folder and not is_blacklisted and not is_git_repo then
-			if vim.env.GIT_WORK_TREE ~= home_dir then
-				vim.api.nvim_echo({ { "Setting git worktree to track bare dotfiles repo." } }, true, {})
-				vim.env.GIT_WORK_TREE = home_dir
-				vim.env.GIT_DIR = home_dir .. "/.dotfiles"
-			end
-		else
-			vim.env.GIT_WORK_TREE = vim.v.null
-			vim.env.GIT_DIR = vim.v.null
-			pcall(vim.api.nvim_set_current_dir, root_dir)
-		end
-	end,
-})
