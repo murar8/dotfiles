2c53390ad2fc3b644efafa012d94c4088f397828
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-01-11 11:12:57 +0100

    use external claude

--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -1,30 +1,12 @@
-local toggle_key = "<C-S-/>"
 return {
 	"coder/claudecode.nvim",
 	optional = true,
 	opts = {
 		terminal = {
-			---@module "snacks"
-			---@type snacks.win.Config|{}
-			snacks_win_opts = {
-				position = "float",
-				width = 0.9,
-				height = 0.9,
-				keys = {
-					claude_hide = {
-						toggle_key,
-						function(self)
-							self:hide()
-						end,
-						mode = "t",
-						desc = "Hide",
-					},
-				},
-			},
+			provider = "none", -- Disable terminal management, use external Claude
 		},
 	},
 	keys = {
-		{ toggle_key, "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-		{ toggle_key, "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+		{ "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
 	},
 }
