ccab9350e73588118857bf24be384ab9af1c8075
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-02-08 10:43:53 +0100

    lazydocker keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 vim.keymap.set("n", "<leader><BS>", function()
 	Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
+
+if vim.fn.executable("lazydocker") == 1 then
+	vim.keymap.set("n", "<leader>k", function()
+		Snacks.terminal("lazydocker")
+	end, { desc = "Lazydocker" })
+end
